2c48f8232d514998cab6f95d6b92d2850578ec11
hands8142 <dongsu8142@naver.com>
2021-10-09 09:56:52 +0900

    feat: Add skipCommand

---
+++ src/commands/skip.ts
@@ -0,0 +1,21 @@
+import { ICommand } from "wokcommands";
+import DiscordClient from "../client";
+
+export default {
+  name: "스킵",
+  category: "Music",
+  description: "노래를 건너뜁니다.",
+  slash: true,
+  guildOnly: true,
+  testOnly: true,
+  callback: ({ client, guild }) => {
+    const player = (client as DiscordClient).player;
+    const queue = player.getQueue(guild!);
+    if (queue) {
+      queue.skip();
+      return "음악을 스킵했습니다.";
+    } else {
+      return "재생중인 음악이 없습니다!";
+    }
+  },
+} as ICommand;
